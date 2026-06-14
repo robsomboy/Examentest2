e04bdf5369e2ca1b7811c3939129d28003a15cbd
robsomboy <robin.wuyts60@gmail.com>
2026-06-14 12:03:34 +0200

    aanpassing conflict-test2

--- Examentest/Program.cs
+++ Examentest/Program.cs
@@ -1,3 +1,3 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
-Console.WriteLine("Nieuwe branch");
+Console.WriteLine("DIt is conflict-test2");
